9bcf90957455261100df062afd2b9c4a6dd7be14
Jubulani2 <jubulani@fastmail.fm>
2017-09-03 19:18:43 +0300

    Import code from existing game

---
+++ main.js
@@ -0,0 +1,67 @@
+// Version 1.0
+
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var roleRepairer = require('role.repairer');
+
+module.exports.loop = function () {
+
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+
+    if(harvesters.length < 3) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, WORK, CARRY, MOVE, MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+    
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    
+    if (upgraders.length < 4) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, {role: 'upgrader'});
+        console.log('Spawning new upgrader: ' + newName);
+    }
+    
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    if (builders.length < 3) {
+        var newName = Game.spawns['Spawn1'].createCreep([CARRY, CARRY, WORK, WORK, MOVE, MOVE], undefined, {role: 'builder'});
+        console.log('Spawning new builder: ' + newName);
+    }
+    
+    var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+    if (repairers.length < 1) {
+        var newName = Game.spawns['Spawn1'].createCreep([CARRY, WORK, MOVE], undefined, {role: 'repairer'});
+        console.log('Spawning new repairer: ' + newName);
+    }
+    
+    if(Game.spawns['Spawn1'].spawning) {
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1,
+            Game.spawns['Spawn1'].pos.y,
+            {align: 'left', opacity: 0.8});
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+        if (creep.memory.role == 'repairer') {
+            roleRepairer.run(creep);
+        }
+    }
+}
---
+++ role.repairer.js
@@ -0,0 +1,44 @@
+
+var roleRepairer = {
+    
+        /** @param {Creep} creep **/
+        run: function(creep) {
+            if (creep.carry.energy == 0) {
+                creep.memory.repairing = false;
+            }
+    
+            if(creep.memory.repairing && creep.carry.energy == 0) {
+                creep.memory.repairing = false;
+                creep.say('🔄 harvest');
+            }
+            if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
+                creep.memory.repairing = true;
+                creep.say('🚧 repairing');
+            }
+            if(creep.memory.repairing) {
+                var targets = _.filter(creep.room.find(FIND_STRUCTURES), (struct) => struct.hits < struct.hitsMax);
+                if(targets.length) {
+                    if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+                else {
+                    creep.moveTo(Game.spawns['Spawn1']);
+                }
+            }
+            else {
+                var sources = creep.room.find(FIND_SOURCES);
+                if (creep.id == '59ab0491914ad16d03244493') {
+                    if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
+                } else {
+                    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
+                }
+            }
+        }
+    };
+    
+    module.exports = roleRepairer;
